cb1f262a8eff428c6e2c6a0a0f815aba4cabf0ad
Mailen Ortiz <mailenortiz.m@gmail.com>
2025-08-23 01:54:38 -0300

    Finalización de trabajo práctico

--- 02 Estructuras Condicionales/tpcondicionales_ortizMailen.py
+++ 02 Estructuras Condicionales/tpcondicionales_ortizMailen.py
@@ -182,64 +182,64 @@ else:
     #HEMISFERIO NORTE --------------------------------------------------------
     if hemisferio == "norte": 
         if (mes == "enero") or (mes == "febrero"): #ENERO / FEBRERO - INVIERNO
-            print("Invierno")
+            print("Estación actual: Invierno")
         elif (mes == "marzo"): #MARZO - INVIERNO / PRIMAVERA
             if (dia <= 20):
-                print("Invierno")
+                print("Estación actual: Invierno")
             elif (dia > 20):
-                print("Primavera")
+                print("Estación actual: Primavera")
         elif (mes == "abril") or (mes == "mayo"): #ABRIL / MAYO - PRIMAVERA
-            print("Primavera")
+            print("Estación actual: Primavera")
         elif (mes == "junio"): #JUNIO - PRIMAVERA / VERANO
             if (dia <= 20):
-                print("Primavera")
+                print("Estación actual: Primavera")
             elif (dia > 20):
-                print("Verano")
+                print("Estación actual: Verano")
         elif (mes == "julio") or (mes == "agosto"): #JULIO / AGOSTO - VERANO
-            print("Verano")
+            print("Estación actual: Verano")
         elif (mes == "septiembre"): #SEPTIEMBRE - VERANO / OTOÑO
             if (dia <= 20):
-                print("Verano")
+                print("Estación actual: Verano")
             elif (dia > 20):
-                print("Otoño")
+                print("Estación actual: Otoño")
         elif (mes == "octubre") or (mes == "noviembre"): #OCTUBRE / NOVIEMBRE - OTOÑO
-            print("Otoño")
+            print("Estación actual: Otoño")
         elif (mes == "diciembre"): #DICIEMBRE - OTOÑO / INVIERNO
             if (dia <= 20):
-                print("Otoño")
+                print("Estación actual: Otoño")
             elif (dia > 20):
-                print("Invierno")
+                print("Estación actual: Invierno")
         else:
             print("Ingrese un mes válido")
     elif hemisferio == "sur": #HEMISFERIO SUR ---------------------------------
         if (mes == "enero") or (mes == "febrero"): #ENERO / FEBRERO - VERANO
-            print("Verano")
+            print("Estación actual: Verano")
         elif (mes == "marzo"): #MARZO - VERANO / OTOÑO
             if (dia <= 20):
-                print("Verano")
+                print("Estación actual: Verano")
             elif (dia > 20):
-                print("Otoño")
+                print("Estación actual: Otoño")
         elif (mes == "abril") or (mes == "mayo"): #ABRIL / MAYO - OTOÑO
-            print("Otoño")
+            print("Estación actual: Otoño")
         elif (mes == "junio"): #JUNIO - OTOÑO / INVIERNO
             if (dia <= 20):
-                print("Otoño")
+                print("Estación actual: Otoño")
             elif (dia > 20):
-                print("Invierno")
+                print("Estación actual: Invierno")
         elif (mes == "julio") or (mes == "agosto"): #JULIO / AGOSTO - INVIERNO
-            print("Invierno")
+            print("Estación actual: Invierno")
         elif (mes == "septiembre"): #SEPTIEMBRE - INVIERNO / PRIMAVERA
             if (dia <= 20):
-                print("Invierno")
+                print("Estación actual: Invierno")
             elif (dia > 20):
-                print("Primavera")
+                print("Estación actual: Primavera")
         elif (mes == "octubre") or (mes == "noviembre"): #OCTUBRE / NOVIEMBRE - PRIMAVERA
-            print("Primavera")
+            print("Estación actual: Primavera")
         elif (mes == "diciembre"): #DICIEMBRE
             if (dia <= 20):
-                print("Primavera")
+                print("Estación actual: Primavera")
             elif (dia > 20):
-                print("Verano")
+                print("Estación actual: Verano")
         else:
             print("Ingrese un mes válido")        
     else:    
