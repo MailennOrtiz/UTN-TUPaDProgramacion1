bee39cba7e06398e37d16a6b488e303dc8580b45
Mailen Ortiz <mailenortiz.m@gmail.com>
2025-08-31 21:18:48 -0300

    Corrección ejercicio 5

--- 03 Estructuras Repetitivas/tprepetitivas_ortizMailen.py
+++ 03 Estructuras Repetitivas/tprepetitivas_ortizMailen.py
@@ -56,3 +56,19 @@ while numero != CORTE:
     numero = int(input(f"Ingrese otro número ({CORTE} - Terminar Suma)"))
 
 print(f"La suma total de los números ingresados es: {sumatoria}")
+
+#5) Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el
+# programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
+
+import random
+numeroAleatorio = random.randint(0, 9)
+numero = int(input("Adivina el número aleatorio. Ingrese el número que crea que es: "))
+
+intentos = 1
+
+while numeroAleatorio != numero:
+    numero = int(input("Incorrecto! Intentalo nuevamente: "))
+
+    intentos += 1
+
+print(f"¡CORRECTO! - Intentos: {intentos}")
