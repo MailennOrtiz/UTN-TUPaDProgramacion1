187efd49ddfa3767e109308ae32f30e12fe0bae8
Mailen Ortiz <mailenortiz.m@gmail.com>
2025-09-01 16:14:08 -0300

    Resolución ejercicio 6 y 7

--- 03 Estructuras Repetitivas/tprepetitivas_ortizMailen.py
+++ 03 Estructuras Repetitivas/tprepetitivas_ortizMailen.py
@@ -72,3 +72,23 @@ while numeroAleatorio != numero:
     intentos += 1
 
 print(f"¡CORRECTO! - Intentos: {intentos}")
+
+# 6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+# entre 0 y 100, en orden decreciente.
+
+for contador in range(100, -1, -2):
+    print(contador) 
+
+# 7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
+# número entero positivo indicado por el usuario.
+
+numero = int(input("Ingrese un número entero positivo:"))
+sumatoria = 0
+
+if numero >= 0:
+    for contador in range(numero + 1):
+        sumatoria += contador
+    print(f"La suma de todos los números comprendidos entre 0 y {numero} es: {sumatoria}")
+else: 
+    print("Número no válido! Debe ingresar un número positivo")
+
